8f55f4f1696761e03c4584d29a581584a34e49c8
Deepak <deepakkumarkakarana@gmail.com>
2024-04-04 23:20:00 +0530

    some changes in cont5

--- src/Components/Cont5.jsx
+++ src/Components/Cont5.jsx
@@ -1,5 +1,8 @@
 import "../Components/Cont5.css";
 import line from "../assets/Line-Design.svg";
+import c51 from "../assets/c51.jpg"
+import c52 from "../assets/c52.webp"
+import c53 from "../assets/c53.webp"
 
 
 export default function Cont5() {
@@ -9,6 +12,17 @@ export default function Cont5() {
         <h1>Top Sellers</h1>
         <p>Love the most to bought the most</p>
         <img src={line} alt="" />
+
+        <h1>New For You!</h1>
+        <p>Our latest releases, just for you !</p>
+        <img src={line} alt="" />
+
+        <div className="cont5smallthree">
+          <div className="c5sm3"><img src={c51} alt="error"/><a href=""><button>Explore More</button></a></div>
+          <div className="c5sm3"><img src={c52} alt="error"/><a href=""><button>Explore More</button></a></div>
+          <div className="c5sm3"><img src={c53} alt="error"/><a href=""><button>Explore More</button></a></div>
+        </div>
+
       </div>
 
     </div>
