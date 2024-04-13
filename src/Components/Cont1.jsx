49ae263624549c1ba84fa8d8be12eaadfc86b1b1
Deepak <deepakkumarkakarana@gmail.com>
2024-04-13 19:11:50 +0530

    front page images changed

--- src/Components/Cont1.jsx
+++ src/Components/Cont1.jsx
@@ -1,10 +1,10 @@
 import "../Components/Cont1.css";
-import slide1 from "../assets/s1.jpg"
-import slide2 from "../assets/s2.jpg"
-import slide3 from "../assets/s3.jpg"
-import slide4 from "../assets/s4.jpg"
-import slide5 from "../assets/s5.jpg"
-import slide6 from "../assets/s6.jpg"
+import slide1 from "../assets/a1.webp"
+import slide2 from "../assets/a2.webp"
+import slide3 from "../assets/a3.jpg"
+import slide4 from "../assets/a4.webp"
+import slide5 from "../assets/a5.webp"
+import slide6 from "../assets/a6.webp"
 
 export default function Cont1() {
   return (
